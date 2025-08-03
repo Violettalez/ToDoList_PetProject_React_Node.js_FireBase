4de5b54bd7148883258409903911469e79b7a46c
Violetta Lezhnenko <leznenkovioletta@gmail.com>
2025-08-03 09:59:48 +0300

    create main pages of project

---
+++ src/frontend/pages/Autorization.jsx
@@ -0,0 +1,9 @@
+function Autorization() {
+  return (
+    <div>
+      <h1>Authorization Page</h1>
+      <p>Please log in to access the application.</p>
+    </div>
+  );
+}
+export default Autorization;
---
+++ src/frontend/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the To-Do List App</h1>
+      <p>This is your home page where you can manage your tasks.</p>
+    </div>
+  );
+}
+export default Home;
