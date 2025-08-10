2093e901554e13e749ce6ea726ecaa65bd850931
Violetta Lezhnenko <leznenkovioletta@gmail.com>
2025-08-10 20:05:39 +0300

    Fix background gradient

--- src/frontend/pages/Home.jsx
+++ src/frontend/pages/Home.jsx
@@ -159,7 +159,7 @@ function Home() {
   });
 
   return (
-    <div className="relative flex flex-col items-center h-full py-[2%] gap-10 bg-gradient-to-b from-bg1 to-accent1">
+    <div className=" min-h-screen relative flex flex-col items-center h-full py-[2%] gap-10 bg-gradient-to-b from-bg1 to-accent1">
       <div className="flex flex-1 flex-row justify-between items-center w-[70%] ">
         <div className="flex items-center gap-4">
           {/* Input date for showing tasks by date*/}
