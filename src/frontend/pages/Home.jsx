8999abd8c4ac2f70c2d5a3e09128a30ace66d129
Violetta Lezhnenko <leznenkovioletta@gmail.com>
2025-08-11 19:59:57 +0300

    Refactor Home component: enhance task filtering with date selection, improve UI elements, and add no tasks found notification

--- src/frontend/pages/Home.jsx
+++ src/frontend/pages/Home.jsx
@@ -8,6 +8,7 @@ import { FaCheck } from "react-icons/fa6";
 import { FiPlus } from "react-icons/fi";
 import { IoExit } from "react-icons/io5";
 import { MdOutlineClose } from "react-icons/md";
+import { FaRegSadCry } from "react-icons/fa";
 
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -23,7 +24,6 @@ function Home() {
     { id: 2, value: "Active", label: "Active" },
     { id: 3, value: "Completed", label: "Completed" },
   ]);
-
   const [optionsCategory, setOptionsCategory] = useState([
     { id: 0, value: "New Category", label: "New Category" },
     { id: 1, value: "All", label: "All" },
@@ -31,7 +31,6 @@ function Home() {
     { id: 3, value: "Home", label: "Home" },
     { id: 4, value: "Study", label: "Study" },
   ]);
-
   const [tasksData, setTasksData] = useState([
     {
       id: 1,
@@ -109,37 +108,32 @@ function Home() {
   const [openStatusList, setOpenStatusList] = useState(false);
   const [openProfile, setOpenProfile] = useState(false);
   const [hoveredId, setHoveredId] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [openCategoryList, setOpenCategoryList] = useState(false);
+  const [selectedCategoryNT, setSelectedCategoryNT] = useState("");
+  const [openCategoryListNT, setOpenCategoryListNT] = useState(false);
+  const today = new Date().toISOString().split("T")[0];
+  const [selectedDate, setSelectedDate] = useState(today);
+  const [isDark, setIsDark] = useState(false);
+  const [openNewTask, setOpenNewTask] = useState(false);
 
   const handleStatusChange = (value) => {
     setSelectedStatus(value);
     setOpenStatusList(false);
   };
-
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [openCategoryList, setOpenCategoryList] = useState(false);
-
   const handleCategoryChange = (value) => {
     setSelectedCategory(value);
     setOpenCategoryList(false);
   };
-
-  const [selectedCategoryNT, setSelectedCategoryNT] = useState("");
-  const [openCategoryListNT, setOpenCategoryListNT] = useState(false);
-
   const handleCategoryChangeNT = (value) => {
     setSelectedCategoryNT(value);
     setOpenCategoryListNT(false);
   };
 
-  const today = new Date().toISOString().split("T")[0];
-  const [selectedDate, setSelectedDate] = useState(today);
-
-  const [isDark, setIsDark] = useState(false);
-
   const getUserTasks = () => {
     try {
       const loginUser = localStorage.getItem("token");
-      //const res = await userData(loginUser);
+      //const res = await userData(loginUser,selectedDate);
       //return res.data.tasks;
     } catch (error) {
       console.error("Error fetching user tasks:", error);
@@ -147,7 +141,6 @@ function Home() {
   };
 
   const changeTaskStatus = (idTask) => {
-    
     // Update the task status in the local state
     setTasksData((prevTasks) =>
       prevTasks.map((task) =>
@@ -160,7 +153,7 @@ function Home() {
       )
     );
 
-    // Update the task status in the backend 
+    // Update the task status in the backend
     try {
       const loginUser = localStorage.getItem("token");
       //const res = await updateTaskStatus(loginUser, idTask);
@@ -178,11 +171,9 @@ function Home() {
     } else {
       document.documentElement.classList.remove("dark");
     }
-  }, [isDark]);
+  }, [isDark],[selectedDate]);
 
   const toggleTheme = () => setIsDark(!isDark);
-
-  const [openNewTask, setOpenNewTask] = useState(false);
 
   //Sorting data
 
@@ -200,72 +191,72 @@ function Home() {
   });
 
   return (
-    <div className=" min-h-screen relative flex flex-col items-center h-full py-[2%] gap-10 bg-gradient-to-b from-bg1 to-accent1">
-      <div className="flex flex-1 flex-row justify-between items-center w-[70%] ">
-        <div className="flex items-center gap-4">
-          {/* Input date for showing tasks by date*/}
-          <input
-            type="date"
-            className="bg-details py-[13px] px-[15px] rounded-xl font-rubik text-xl h-full w-[195px] max-w-xs text-bg1 cursor-pointer"
-            value={selectedDate}
-            onChange={(e) => setSelectedDate(e.target.value)}
-          />
+    <div className=" min-h-screen relative flex flex-col items-center gap-4 bg-gradient-to-b from-bg1 to-accent1">
+      <div className="flex flex-1 flex-row items-center justify-between md:gap-4 md:justify-start flex-wrap w-[95%] md:w-[70%]">
+        {/* Input date for showing tasks by date*/}
+        <input
+          type="date"
+          className="bg-details my-2 py-[13px] px-[15px] rounded-xl font-rubik text-base md:text-xl h-full w-[49%] md:w-[195px] max-w-xs text-bg1 cursor-pointer"
+          value={selectedDate}
+          onChange={(e) => setSelectedDate(e.target.value)}
+        />
 
-          {/* Input status*/}
-          <div className="relative inline-block w-[195px] max-w-xs cursor-pointer">
-            <div
-              className="text-text bg-bg1 py-[13px] px-[15px] pr-4 rounded-xl font-rubik text-xl appearance-none w-[195px] flex items-center justify-between shadow-main "
-              onClick={() => setOpenStatusList(!openStatusList)}
-            >
-              {selectedStatus === "" ? "Status" : selectedStatus}
-              {openStatusList && <IoIosArrowDropupCircle />}
-              {!openStatusList && <IoIosArrowDropdownCircle />}
-            </div>
-            {openStatusList && (
-              <ul className="absolute z-10 w-full bg-bg1 rounded-b-xl shadow-main mt-1">
-                {optionsStatus.map((option) => (
+        {/* Input status*/}
+        <div className="relative inline-block w-[49%] md:w-[195px] max-w-xs cursor-pointer">
+          <div
+            className="text-text bg-bg1 py-[13px] px-[15px] pr-4 rounded-xl font-rubik text-base md:text-xl appearance-none flex items-center justify-between shadow-main "
+            onClick={() => setOpenStatusList(!openStatusList)}
+          >
+            {selectedStatus === "" ? "Status" : selectedStatus}
+            {openStatusList && <IoIosArrowDropupCircle />}
+            {!openStatusList && <IoIosArrowDropdownCircle />}
+          </div>
+          {openStatusList && (
+            <ul className="absolute z-10 w-full bg-bg1 rounded-b-xl shadow-main mt-1">
+              {optionsStatus.map((option) => (
+                <li
+                  key={option.id}
+                  value={option.value}
+                  onClick={() => handleStatusChange(option.value)}
+                  className="text-text bg-bg1 py-[13px] px-[15px]  font-rubik text-l cursor-pointer hover:bg-accent1 hover:text-static-text rounded-b-xl"
+                >
+                  {option.label}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+
+        {/* Input category*/}
+        <div className="relative inline-block w-[49%] md:w-[195px] max-w-xs cursor-pointer">
+          <div
+            className="text-text bg-bg1 py-[13px] px-[15px] pr-4 rounded-xl font-rubik text-base md:text-xl appearance-none flex items-center justify-between shadow-main"
+            onClick={() => setOpenCategoryList(!openCategoryList)}
+          >
+            {selectedCategory === "" ? "Category" : selectedCategory}
+            {openCategoryList && <IoIosArrowDropupCircle />}
+            {!openCategoryList && <IoIosArrowDropdownCircle />}
+          </div>
+          {openCategoryList && (
+            <ul className="absolute z-10 w-full bg-bg1 mt-1 shadow-main rounded-b-xl">
+              {optionsCategory
+                .filter((option) => option.id !== 0)
+                .map((option) => (
                   <li
                     key={option.id}
                     value={option.value}
-                    onClick={() => handleStatusChange(option.value)}
-                    className="text-text bg-bg1 py-[13px] px-[15px]  font-rubik text-l cursor-pointer hover:bg-accent1 hover:text-static-text rounded-b-xl"
+                    onClick={() => handleCategoryChange(option.value)}
+                    className="text-text bg-bg1 py-[13px] px-[15px]  font-rubik text-l cursor-pointer rounded-b-xl hover:bg-accent1 hover:text-static-text"
                   >
                     {option.label}
                   </li>
                 ))}
-              </ul>
-            )}
-          </div>
-
-          {/* Input category*/}
-          <div className="relative inline-block w-[195px] max-w-xs cursor-pointer">
-            <div
-              className="text-text bg-bg1 py-[13px] px-[15px] pr-4 rounded-xl font-rubik text-xl appearance-none w-[195px] flex items-center justify-between shadow-main"
-              onClick={() => setOpenCategoryList(!openCategoryList)}
-            >
-              {selectedCategory === "" ? "Category" : selectedCategory}
-              {openCategoryList && <IoIosArrowDropupCircle />}
-              {!openCategoryList && <IoIosArrowDropdownCircle />}
-            </div>
-            {openCategoryList && (
-              <ul className="absolute z-10 w-full bg-bg1 mt-1 shadow-main rounded-b-xl">
-                {optionsCategory
-                  .filter((option) => option.id !== 0)
-                  .map((option) => (
-                    <li
-                      key={option.id}
-                      value={option.value}
-                      onClick={() => handleCategoryChange(option.value)}
-                      className="text-text bg-bg1 py-[13px] px-[15px]  font-rubik text-l cursor-pointer rounded-b-xl hover:bg-accent1 hover:text-static-text"
-                    >
-                      {option.label}
-                    </li>
-                  ))}
-              </ul>
-            )}
-          </div>
+            </ul>
+          )}
         </div>
-        <div className="flex items-center gap-4">
+
+        {/* Dark mode toggle and profile button*/}
+        <div className="flex md:flex-2 items-center justify-around md:justify-end md:gap-4 w-[49%] md:w-[195px]">
           {/* Switch light and dark mode*/}
           <button
             onClick={toggleTheme}
@@ -289,12 +280,14 @@ function Home() {
           {/* Profile button*/}
           <div className="relative flex">
             <div
-              className="flex items-center gap-4"
+              className="flex items-center gap-2 md:gap-4"
               onClick={() => setOpenProfile(!openProfile)}
             >
-              <div className="h-10 aspect-square rounded-full bg-[url(/persone.png)] bg-cover bg-center bg-no-repeat"></div>
+              <div className="h-7 md:h-10 aspect-square rounded-full bg-[url(/persone.png)] bg-cover bg-center bg-no-repeat"></div>
               {/* This is a placeholder for user photo */}
-              <p className="text-signika text-xl text-text">Login</p>{" "}
+              <p className="text-signika text-base md:text-xl text-text">
+                Login
+              </p>{" "}
               {/* This is a placeholder for user login */}
             </div>
 
@@ -308,8 +301,10 @@ function Home() {
                       navigate("/");
                     }}
                   >
-                    <IoExit className="text-accent2 text-base font-signika" />
-                    <p className="text-text text-base font-signika">Sign out</p>
+                    <IoExit className="hidden md:block text-accent2 text-base font-signika" />
+                    <p className="text-accent2 md:text-text text-center text-base font-signika">
+                      Sign out
+                    </p>
                   </li>
                 </ul>
               </div>
@@ -319,21 +314,21 @@ function Home() {
       </div>
 
       {/* Main container with tasks*/}
-      <div className="flex-10 w-[70%] bg-gradient-to-b from-accent1 to-bg1 flex flex-col items-center rounded-3xl shadow-main px-25 py-[3%] gap-5">
+      <div className="flex-10 w-[95%] md:w-[70%] bg-gradient-to-b from-accent1 to-bg1 flex flex-col items-center rounded-3xl shadow-main px-[3%] md:px-25 py-[3%] gap-5">
         <div className="flex flex-row items-center w-full justify-between">
-          <div className="w-[50px]"></div>
+          <div className="w-[40px]"></div>
 
           {/* Main logo*/}
           <img
             src="/logo.svg"
             alt="logo"
-            className="w-[150px] logo-fil-static"
+            className="w-[100px] md:w-[150px] logo-fil-static"
           />
 
           {/* Add new task button*/}
           <button
             onClick={() => setOpenNewTask(!openNewTask)}
-            className="w-[50px] aspect-square bg-bg1 shadow-main flex items-center justify-center cursor-pointer rounded-full hover:bg-static-details transition duration-300"
+            className="w-[40px] md:w-[50px] aspect-square bg-bg1 shadow-main flex items-center justify-center cursor-pointer rounded-full hover:bg-static-details transition duration-300"
           >
             <FiPlus className="text-xl text-text" />
           </button>
@@ -342,9 +337,14 @@ function Home() {
         {/*Main table with tasks*/}
         <div className="flex flex-col gap-4 justify-center w-full">
           {filteredTasks.length === 0 ? (
-            <p className="text-text text-xl font-signika text-center">
-              No tasks found
-            </p>
+            <div className="flex flex-col items-center justify-center gap-1">
+              <p className="text-text text-3xl font-signika text-center">
+                <FaRegSadCry />
+              </p>
+              <p className="text-text text-base md:text-xl font-signika text-center">
+                No tasks found
+              </p>
+            </div>
           ) : (
             filteredTasks.map((task) => (
               <div
@@ -366,7 +366,7 @@ function Home() {
 
                   {/*Title*/}
                   <p
-                    className={`font-signika text-base font-semibold text-text ${
+                    className={`font-signika text-sm md:text-base font-semibold text-text ${
                       task.status === "Completed" && "line-through"
                     }`}
                   >
@@ -375,20 +375,20 @@ function Home() {
                 </div>
 
                 {/*Addition information about task*/}
-                <div className="flex items-center gap-2 w-[30%] justify-between">
+                <div className="flex items-center gap-2 md:w-[30%] justify-between">
                   <div className="flex items-center justify-start gap-2">
                     <p>|</p>
-                    <p className="text-base font-rubik text-details font-bold">
+                    <p className="text-sm md:text-base font-rubik text-details font-bold">
                       #{task.category}
                     </p>
                   </div>
-                  <p className="opacity-80 text-text font-rubik text-base">
+                  <p className="hidden md:block opacity-80 text-text font-rubik text-base">
                     {task.date}
                   </p>
 
                   {/*Edit button and delete button*/}
                   {hoveredId === task.id && (
-                    <div className=" flex items-center gap-2">
+                    <div className="flex items -center gap-2">
                       <button>
                         <FaEdit className="text-text text-base cursor-pointer" />
                       </button>
